cd16158030439e43e2aac8df741002013d4d83c9
AmrSaleh2234 <amrsaleh@gmail.com>
2023-07-03 19:11:43 +0300

    first commit

---
+++ resources/views/includes/search_form.blade.php
@@ -0,0 +1,36 @@
+@if(Auth::guard('company')->check())
+<form action="{{route('job.seeker.list')}}" method="get">
+    <div class="searchbar">
+		<div class="srchbox seekersrch">		
+		<div class="input-group">
+		  <input type="text"  name="search" id="empsearch" value="{{Request::get('search', '')}}" class="form-control" placeholder="{{__('Enter Skills or Job Seeker Details')}}" autocomplete="off" />
+		  <span class="input-group-btn">
+			<input type="submit" class="btn" value="{{__('Search Job Seeker')}}">
+		  </span>
+		</div>
+		</div>
+
+        
+    </div>
+</form>
+@else
+
+
+
+	
+		<div class="searchbar">
+			<form action="{{route('job.list')}}" method="get">	
+			<div class="input-group">
+			<input type="text"  name="search" id="jbsearch" value="{{Request::get('search', '')}}" class="form-control" placeholder="{{__('Enter Skills or job title')}}" autocomplete="off" />
+			{!! Form::select('country_id[]', ['' => __('Select Country')]+$countries, array('class'=>'form-control', 'id'=>'functional_area_id')) !!}
+{{--				,'value'=>auth()->user()->country_id--}}
+			<button type="submit" class="btn"><i class="fas fa-search"></i></button>
+			</div>						
+			</form>
+    		</div>
+
+	
+	
+	 
+
+@endif
